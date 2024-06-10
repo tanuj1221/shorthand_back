f69454dfe4b128ec4b43cb892427d2c8a835907b
tanuj1221 <75058861+tanuj1221@users.noreply.github.com>
2024-06-10 10:30:24 +0530

    uodated payments
    dfd

--- my-app/src/components/InstituteDashboard.js
+++ my-app/src/components/InstituteDashboard.js
@@ -11,7 +11,7 @@ const InstituteDashboard = ({ setIsAuthenticated }) => {
   const [instituteName, setInstituteName] = useState('Loading Institute...'); // Initial placeholder
   const [instituteId, setInstituteId] = useState('Loading ID');
   const navigate = useNavigate();
-  fetch('http://localhost:3000/institutedetails', {
+  fetch('http://15.206.160.1:3000/institutedetails', {
     method: 'GET',
     headers: {
       'Authorization': `Bearer ${localStorage.getItem('token')}`,
@@ -37,7 +37,7 @@ const InstituteDashboard = ({ setIsAuthenticated }) => {
   const handleLogout = async () => {
     try {
       // Send a request to the backend to destroy the session
-      await axios.post('http://localhost:3000/logoutinsti');
+      await axios.post('http://15.206.160.1:3000/logoutinsti');
 
       setIsAuthenticated(false);
       // Redirect to the login page
